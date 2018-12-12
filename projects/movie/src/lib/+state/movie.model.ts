960b22228c3fc2cd121624bb1cf32bd2bac3be45
Jackseed <vinkho.c@gmail.com>
2018-12-12 14:41:30 +0100

    initial setup

---
+++ projects/movie/src/lib/+state/movie.model.ts
@@ -0,0 +1,11 @@
+import { ID } from '@datorama/akita';
+
+export interface Movie {
+  id: ID;
+}
+
+export function createMovie(params: Partial<Movie>) {
+  return {
+
+  } as Movie;
+}
---
+++ projects/movie/src/lib/movie.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-movie',
+  template: `
+    <p>
+      movie works!
+    </p>
+  `,
+  styles: []
+})
+export class MovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
